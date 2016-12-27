7d6cf65e39a1f89ca5a151a48e57c09fd489bbab
jbumbay159 <jaybeeumbay159@outlook.com>
2016-12-27 13:52:08 +0800

    Change Course
    Course to Courses

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,7 +18,7 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect('/course');
+            return redirect('/courses');
         }
 
         return $next($request);
