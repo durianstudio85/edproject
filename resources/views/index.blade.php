afe0efc478bf061f9acc4cac3a1397d0e9a5b54f
jbumbay159 <jaybeeumbay159@outlook.com>
2017-02-09 17:36:08 +0800

    Add Google Analytics
    Google Analytics

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,6 +12,16 @@
 	<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/font-awesome.css') }}">
 	<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/bootstrap.css') }} ">
 	<link href='https://fonts.googleapis.com/css?family=Signika:400,600,300,700' rel='stylesheet' type='text/css'>
+	<script>
+         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+         })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+         ga('create', 'UA-44336723-1', 'auto');
+         ga('send', 'pageview');
+
+    </script>
 </head>
 <body>
   <!-- Static navbar -->
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,6 +22,17 @@
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/bootstrap.css') }} ">
     <link href="{{ asset('assets/css/style2.css') }}" rel="stylesheet">
 
+    <script>
+         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+         })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+         ga('create', 'UA-44336723-1', 'auto');
+         ga('send', 'pageview');
+
+    </script>
+
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 
